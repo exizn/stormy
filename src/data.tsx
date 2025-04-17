eae229f67cd86661ab37ebca8273b3717d2ebc61
exizn <erheserhes126@gmail.com>
2025-04-17 10:36:38 +0800

    Save project to igreplica branch

---
+++ src/data.tsx
@@ -0,0 +1,14 @@
+export const postData = [
+    { id: 1, desc: "My post1", img: require("../assets/images/Accountlcon2.png") },
+    { id: 2, desc: "My post2", img: require("../assets/images/Accountlcon2.png") },
+    { id: 3, desc: "My post3", img: require("../assets/images/Accountlcon2.png") },
+    { id: 4, desc: "My post1", img: require("../assets/images/Accountlcon2.png") },
+    { id: 5, desc: "My post2", img: require("../assets/images/Accountlcon2.png") },
+    { id: 6, desc: "My post3", img: require("../assets/images/Accountlcon2.png") },
+    { id: 1, desc: "My post1", img: require("../assets/images/Accountlcon2.png") },
+    { id: 2, desc: "My post2", img: require("../assets/images/Accountlcon2.png") },
+    { id: 3, desc: "My post3", img: require("../assets/images/Accountlcon2.png") },
+    { id: 4, desc: "My post1", img: require("../assets/images/Accountlcon2.png") },
+    { id: 5, desc: "My post2", img: require("../assets/images/Accountlcon2.png") },
+    { id: 6, desc: "My post3", img: require("../assets/images/Accountlcon2.png") },
+  ];
